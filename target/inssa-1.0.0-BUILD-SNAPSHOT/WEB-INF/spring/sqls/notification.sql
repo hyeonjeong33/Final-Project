4dc7072f90be5dbe70af32200192745c3fc3dbc4
hyeonjeong33 <hjhj_93@naver.com>
2019-12-02 14:56:59 +0900

    fixed untracked files

--- target/inssa-1.0.0-BUILD-SNAPSHOT/WEB-INF/spring/sqls/notification.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE NOTIFICATION;
-
-create table notification(
-      username VARCHAR2(20) NOT NULL,
-      friend VARCHAR2(300),
-      allow  VARCHAR2(100) DEFAULT 'N'
-)
-
-SELECT * FROM notification;
